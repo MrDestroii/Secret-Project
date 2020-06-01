e14068e64d8e80f34e71dc80d93a8e12c02530d9
Andrew <mrdestroii@daotech.ru>
2020-06-01 16:56:01 +0400

    fix logout when token expired

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -38,7 +38,7 @@ const basic = (url, method, data, params) => {
     .then((response) => response.data)
     .catch(e => {
       const isLogged = authSelectors.getIsLogged(store.getState())
-      const isUnauthorized = R.compose(R.equals('Unauthorized'), R.path(['response', 'data', 'message']))(e)
+      const isUnauthorized = R.compose(R.equals(401), R.path(['response', 'data', 'statusCode']))(e)
 
       if(isLogged && isUnauthorized) {
         store.dispatch(authActions.logout())
